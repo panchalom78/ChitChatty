5bcd82ba14ef1bd02e995b970aef3a8a4d3bff74
panchalom78 <panchalom787@gmail.com>
2025-01-05 22:38:05 +0530

    final2

--- server/index.js
+++ server/index.js
@@ -32,6 +32,13 @@ const options = [
 
   app.use(options)
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://chitchatty.vercel.app'); // Replace with your actual origin
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  });
+
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
